2b24fdabab7c6c64254179b69f2d65cb649248f8
RODRIGO OLIVEIRA <rodrigo.oliveira85@fatec.sp.gov.br>
2020-09-04 20:00:00 -0300

    Primeiro commit

---
+++ MinhaPrimeiraWebAPI/Controllers/StortiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaPrimeiraWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StortiController : Controller
+    {
+        // GET api/Storti
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/Storti/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "O número digitado foi: " +  id.ToString();
+        }
+    }
+}
